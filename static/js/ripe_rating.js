585bbb8d0d9f02a8b171de0b2d6efd5fe496c115
Redeat Bekele <redeat17@gmail.com>
2021-02-11 22:40:43 -0500

    adding ripe rate color

--- static/js/ripe_rating.js
+++ static/js/ripe_rating.js
@@ -48,16 +48,16 @@ function showData(service) {
             gauge: {
                 axis: { range: [null, 10] },
                 steps: [
-                    { range: [0, 1], color: "rgb(245, 249, 250)" },
-                    { range: [1, 2], color: "rgb(225, 233, 235)" },
-                    { range: [2, 3], color: "rgb(202, 216, 219)" },
-                    { range: [3, 4], color: "rgb(184, 207, 212)" },
-                    { range: [4, 5], color: "rgb(174, 203, 209)" },
-                    { range: [5, 6], color: "rgb(163, 203, 212)" },
-                    { range: [6, 7], color: "rgb(154, 207, 219)" },
-                    { range: [7, 8], color: "rgb(154, 214, 227)" },
-                    { range: [8, 9], color: "rgb(146, 217, 232)" },
-                    { range: [9, 10], color: "rgb(142, 227, 245)" }
+                    { range: [0, 1], color: "rgb(60, 54, 0)" },
+                    { range: [1, 2], color: "rgb(79, 73, 2)" },
+                    { range: [2, 3], color: "rgb(99, 93, 3)" },
+                    { range: [3, 4], color: "rgb(119, 114, 2)"},
+                    { range: [4, 5], color: "rgb(140, 136, 2)"},
+                    { range: [5, 6], color: "rgb(161, 158, 2)"},
+                    { range: [6, 7], color: "rgb(182, 181, 1)"},
+                    { range: [7, 8], color: "rgb(202, 205, 1)"},
+                    { range: [8, 9], color: "rgb(223, 230, 2)"},
+                    { range: [9, 10], color: "rgb(244, 255, 4)"}
                 ]
             }
             // delta: {reference: 400},
@@ -70,3 +70,12 @@ function showData(service) {
 function optionChanged() {
     getServiceData();
 }
+
+
+
+
+
+
+
+
+
